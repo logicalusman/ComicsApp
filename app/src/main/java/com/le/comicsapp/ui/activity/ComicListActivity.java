60dd3746d4d73c6de7d212103d18c9abe6154f89
logicalusman <linkusman21@gmail.com>
2017-07-17 18:54:41 +0100

    Implemented viewmodel pattern for the ComicListActivity.
    The viewmodel exposes an Observable for getting comics list via network
    interface.

--- app/src/main/java/com/le/comicsapp/ui/activity/ComicListActivity.java
+++ app/src/main/java/com/le/comicsapp/ui/activity/ComicListActivity.java
@@ -1,29 +1,165 @@
 package com.le.comicsapp.ui.activity;
 
-import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.le.comicsapp.R;
+import com.le.comicsapp.util.UiUtils;
 import com.le.comicsapp.viewmodel.ComicListViewModel;
+import com.le.comicsapp.viewmodel.data.AppError;
+import com.le.comicsapp.viewmodel.data.ComicItem;
+
+import java.util.List;
+
+import rx.Subscriber;
+import rx.Subscription;
 
 public class ComicListActivity extends CommonActivity {
 
-    private RecyclerView mComicsList;
+    private RecyclerView mComicList;
     private ComicListViewModel mViewModel;
+    private ComicListAdapter mComicListAdapter;
+    private Subscription mSubscription;
+    private AlertDialog mAlertDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comic_list);
-        mComicsList = (RecyclerView) findViewById(R.id.comics_rv);
-        LinearLayoutManager llm = new LinearLayoutManager(this);
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
         mViewModel = ViewModelProviders.of(this).get(ComicListViewModel.class);
+        initComicList();
+        mSubscription = mViewModel.getComicItemObservable().subscribe(new ComicListSubscriber());
+        showProcessingDialog(getString(R.string.fetching_comic_list));
         mViewModel.getComicListAsync();
 
+    }
+
+    private void initComicList() {
+        mComicList = (RecyclerView) findViewById(R.id.comics_rv);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        mComicList.setLayoutManager(llm);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mComicList.getContext(),
+                LinearLayoutManager.VERTICAL);
+        mComicList.addItemDecoration(dividerItemDecoration);
+
+    }
+
+    private void populateComicList(List<ComicItem> comicItemsList) {
+        mComicListAdapter = new ComicListAdapter(comicItemsList);
+        mComicList.setAdapter(mComicListAdapter);
+    }
+
+    private class ComicListSubscriber extends Subscriber<List<ComicItem>> {
+
+        @Override
+        public void onCompleted() {
+
+        }
+
+        @Override
+        public void onNext(List<ComicItem> comicItemsList) {
+            dismissProcessingDialog();
+            if (comicItemsList == null) {
+                showNoItemFoundDialog();
+                return;
+            }
+
+            populateComicList(comicItemsList);
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            dismissProcessingDialog();
+            if (e instanceof AppError) {
+                AppError err = (AppError) e;
+                if (err.getError() == AppError.ERROR_INTERNET_CONNECTION) {
+                    UiUtils.showConnectionAlertErrDialog(ComicListActivity.this);
+                } else {
+                    UiUtils.showUnknownErrDialog(ComicListActivity.this);
+                }
+            }
+        }
+    }
+
+    private class ComicListAdapter extends RecyclerView.Adapter<ComicListAdapter.VH> {
+
+        private List<ComicItem> mComicItemsList;
+
+        public ComicListAdapter(@NonNull List<ComicItem> comicItemsList) {
+            mComicItemsList = comicItemsList;
+        }
+
+
+        @Override
+        public VH onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(ComicListActivity.this).inflate(R.layout.comic_item_row, parent, false);
+            return new VH(v);
+        }
+
+        @Override
+        public void onBindViewHolder(VH holder, int position) {
+            ComicItem item = mComicItemsList.get(position);
+            holder.mTitleTv.setText(item.getTitle());
+            Glide.with(ComicListActivity.this).load(item.getThumbnail()).into(holder.mThumbnailIv);
+        }
+
+        @Override
+        public int getItemCount() {
+            if (mComicItemsList == null) {
+                return 0;
+            } else {
+                return mComicItemsList.size();
+            }
+        }
+
+        class VH extends RecyclerView.ViewHolder {
+
+            private TextView mTitleTv;
+            private ImageView mThumbnailIv;
+
+            public VH(View v) {
+                super(v);
+                mTitleTv = (TextView) v.findViewById(R.id.title_tv);
+                mThumbnailIv = (ImageView) v.findViewById(R.id.thumbnail_iv);
+            }
+        }
+    }
+
+    private void showNoItemFoundDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this).setCancelable(false)
+                .setMessage(R.string.no_comic_items_found).
+                        setPositiveButton(R.string.ok, null);
+
+        mAlertDialog = builder.create();
+        mAlertDialog.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // This will make sure the dialog is dismissed when the activity finishes while
+        // the dialog is in progress, e.g. when the screen is rotated.
+        dismissProcessingDialog();
+        if (mAlertDialog != null && mAlertDialog.isShowing()) {
+            mAlertDialog.dismiss();
+        }
+        if (mSubscription != null) {
+            mSubscription.unsubscribe();
+            mSubscription = null;
+        }
     }
 }
--- app/src/main/java/com/le/comicsapp/viewmodel/ComicListViewModel.java
+++ app/src/main/java/com/le/comicsapp/viewmodel/ComicListViewModel.java
@@ -2,28 +2,52 @@ package com.le.comicsapp.viewmodel;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.le.comicsapp.R;
 import com.le.comicsapp.network.ApiService;
 import com.le.comicsapp.network.RestAdapter;
+import com.le.comicsapp.network.data.ComicAuthor;
+import com.le.comicsapp.network.data.ComicData;
+import com.le.comicsapp.network.data.GetComicsResponse;
 import com.le.comicsapp.util.Utils;
+import com.le.comicsapp.viewmodel.data.AppError;
+import com.le.comicsapp.viewmodel.data.ComicItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Retrofit;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
+import rx.subjects.PublishSubject;
 
 public class ComicListViewModel extends AndroidViewModel {
 
     private String TAG = "ComicListViewModel";
+    private PublishSubject<List<ComicItem>> mComicItemsSubject = PublishSubject.create();
     private Subscription mSubscription;
+    private boolean mComicsRequested;
+    private List<ComicItem> mComicItemsList;
 
     public ComicListViewModel(Application application) {
         super(application);
     }
 
+    public PublishSubject<List<ComicItem>> getComicItemObservable() {
+        return mComicItemsSubject;
+    }
+
     public void getComicListAsync() {
+
+        // If the items were network requested earlier, they must be cached - just use them.
+        if (mComicsRequested) {
+            mComicItemsSubject.onNext(mComicItemsList);
+            return;
+        }
+
         Retrofit retrofit = RestAdapter.createRetrofit(getApplication());
         ApiService apiService = retrofit.create(ApiService.class);
         String apiPubkey = getApplication().getString(R.string.marvel_api_pub_key);
@@ -44,17 +68,53 @@ public class ComicListViewModel extends AndroidViewModel {
         mSubscription = apiService.getComics(apiPubkey, ts, hash, limit).subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread()).subscribe(
                         (response) -> {
-
-
+                            mComicsRequested = true;
+                            // check network error
+                            if (!response.isSuccessful()) {
+                                mComicItemsSubject.onError((new AppError(AppError.ERROR_INTERNET_CONNECTION)));
+                                return;
+                            }
+                            // check the response data
+                            if (response.body() == null) {
+                                mComicItemsSubject.onNext(null);
+                                return;
+                            }
+                            // process response data and pass it to the view
+                            mComicItemsSubject.onNext(processComicData(response.body()));
                         },
 
                         throwable -> {
-
                             Log.e(TAG, throwable.getMessage(), throwable);
-
+                            mComicItemsSubject.onError((new AppError(throwable, AppError.ERROR_UNKNOWN)));
+                            mComicsRequested = true;
                         }
                 );
 
+    }
+
+    private List<ComicItem> processComicData(@NonNull GetComicsResponse response) {
+
+        List<ComicItem> items = new ArrayList<>(response.responseData.comicDataList.size());
+        for (ComicData data : response.responseData.comicDataList) {
+            ComicItem ci = new ComicItem();
+            ci.setTitle(data.title);
+            ci.setDescription(data.description);
+            ci.setNumPages(data.pageCount);
+            if (data.prices != null && !data.prices.isEmpty()) {
+                ci.setPrice(data.prices.get(0).price);
+            }
+            String thumbnail = String.format("%s.%s", data.thumbnail.path, data.thumbnail.extension);
+            ci.setThumbnail(thumbnail);
+            if (data.author.numAuthors > 0) {
+                for (ComicAuthor.Author author : data.author.authorList) {
+                    ci.addAuthor(author.name);
+                }
+            }
+
+            items.add(ci);
+        }
+        mComicItemsList = items;
+        return items;
     }
 
     @Override
